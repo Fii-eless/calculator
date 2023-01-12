64546a73ecdefb6a7b7308c2904984fc58d35098
fii-eless <fiifiad@gmail.com>
2023-01-13 00:00:58 +0400

    Add backspace and clear functions with both keypad and click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const buttons = document.querySelectorAll(".button");
-let displayMinor = [];
+const screenMinor = document.querySelector("#screenMinor");
 let displayMajor = [];
-let input = "";
+let input = [];
+const operators = ["!", "^", "+", "-", "/", "*", "="];
 
 buttons.forEach(button => button.addEventListener("mouseover", Highlight));
 function Highlight(e) {
@@ -16,17 +17,38 @@ function returnKey(e) {
     const key = document.querySelector(`.button[data-key="${e.keyCode}"]`);
     const special = document.querySelector(`.button[data-code="${e.keyCode}"]`);
     if (!key && !special) return;
-    if (!key) input += e.key;
-    if (key) input += e.key;
+    if (e.key === "Backspace") input.splice(input.indexOf("Backspace"), 2);
+    if (!key) input.push(e.key);
+    if (key) input.push(e.key);
+    if (input.includes("Enter")) input[input.indexOf("Enter")] = "=";
+    if (e.key === "Backspace") backSpace(input);
+    if (e.key === "Escape") clear(input);
+    console.log(input)
 }
 buttons.forEach(button => button.addEventListener("click", logXter));
 function logXter(e) {
-  if (!this.textContent) return;
-  if (this.textContent == "Xy")  this.textContent = "^";
-  if (this.textContent == "X!") this.textContent = "!";
-  if (this.textContent == "+/-") this.textContent = "-";
-  input += this.textContent;
+  // if (!this.textContent) return;
+  input.push(this.textContent);
+  if (input.includes("×")) input[input.indexOf("×")] = "*";
+  if (input.includes("÷")) input[input.indexOf("÷")] = "/";
+  if (input.includes("−")) input[input.indexOf("−")] = "-";
+  if (input.includes("Xy")) input[input.indexOf("Xy")] = "^";
+  if (input.includes("X!")) input[input.indexOf("X!")] = "!";
+  if (input.includes("+/-")) input[input.indexOf("+/-")] = "-";
+  if (this.id === "clearAll") clear(input);
+  if (this.id === "backspace") backSpace2(input);
   console.log(input)
+  
+  // return input
+}
+function backSpace(arr) {
+  arr.splice(arr.indexOf("Backspace"), 2);
+}
+function backSpace2(arr) {
+  arr.splice(arr.length - 2, 2);
+}
+function clear(arr) {
+  for (let i = arr.length; i > 0; i-- ) arr.pop();
 }
 
 const add = function(a, b) {
