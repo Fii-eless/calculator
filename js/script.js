54f777adc1c666077ffd35d3c1be55c993c9c596
fii-eless <fiifiad@gmail.com>
2023-01-18 00:18:04 +0400

    Add input format functions that convert input into arguments for operations

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ const buttons = document.querySelectorAll(".button");
 const screenMinor = document.querySelector("#screenMinor");
 let displayMajor = [];
 let input = [];
-const operators = ["!", "^", "+", "-", "/", "*", "="];
+let inputClone = [];
+const operators = ["!", "^", "+", "-", "/", "*"];
+const arithM = ["+", "-"];
+const geoM = ["/", "*", "^", "!"];
+const equator = "=";
+let operationArgs = [];
+let operationRslt = [];
 
 buttons.forEach(button => button.addEventListener("mouseover", Highlight));
 function Highlight(e) {
@@ -17,13 +23,22 @@ function returnKey(e) {
     const key = document.querySelector(`.button[data-key="${e.keyCode}"]`);
     const special = document.querySelector(`.button[data-code="${e.keyCode}"]`);
     if (!key && !special) return;
-    if (e.key === "Backspace") input.splice(input.indexOf("Backspace"), 2);
-    if (!key) input.push(e.key);
-    if (key) input.push(e.key);
-    if (input.includes("Enter")) input[input.indexOf("Enter")] = "=";
-    if (e.key === "Backspace") backSpace(input);
-    if (e.key === "Escape") clear(input);
-    console.log(input)
+    if (e.key === "Backspace") {
+      input.splice(input.indexOf("Backspace"), 2);
+      inputClone.splice(inputClone.indexOf("Backspace"), 2)
+    };
+    if (!key) {input.push(e.key); inputClone.push(e.key);}
+    if (key) {input.push(e.key); inputClone.push(e.key);}
+    if (input.includes("Enter")) {input[input.indexOf("Enter")] = "="; inputClone[inputClone.indexOf("Enter")] = "=";}
+    if (e.key === "Backspace") {backSpace(input); backSpace(inputClone);}
+    
+    formatInput(input);
+    getResult();
+    if (e.key === "Escape") {clear(input); clear(inputClone); clear(operationArgs); clear(operationRslt)}
+    console.log(input);
+    // console.log(inputClone);
+    console.log(operationArgs);
+    console.log(operationRslt);
 }
 buttons.forEach(button => button.addEventListener("click", logXter));
 function logXter(e) {
@@ -38,8 +53,6 @@ function logXter(e) {
   if (this.id === "clearAll") clear(input);
   if (this.id === "backspace") backSpace2(input);
   console.log(input)
-  
-  // return input
 }
 function backSpace(arr) {
   arr.splice(arr.indexOf("Backspace"), 2);
@@ -50,13 +63,77 @@ function backSpace2(arr) {
 function clear(arr) {
   for (let i = arr.length; i > 0; i-- ) arr.pop();
 }
+function formatInput(input) {
+  for (let i=0; i < input.length; i++) {
+    if (input[0] === "=" && operationArgs.length > 1) {
+      operationRslt[0] = operationArgs[0];
+      clear(operationArgs); clear(input);
+    } else if (input[0] === "=") {
+      operationRslt[0] = 0;
+      clear(input);
+    }  else if (arithM.includes(input[0]) && !operationArgs[0]) {
+        operationArgs[0] = 0; operationArgs.push(input[0]);
+        clear(input);
+    } else if (arithM.includes(input[0]) && operationArgs[0]) {
+        operationArgs.push(input[0]);
+        clear(input);
+    } else if (geoM.includes(input[0]) && !operationArgs[0]) {
+        operationArgs[0] = 1; operationArgs.push(input[0]);
+        clear(input);
+    } else if (geoM.includes(input[0]) && operationArgs[0]) {
+      operationArgs.push(input[0]);
+      clear(input);
+    } else if (operators.includes(input[i]) || equator.includes(input[i])) { 
+        operationArgs.push(parseFloat(input.splice(0, input.length - 1).join("")));
+        operationArgs.push(input[input.length -1]);
+        clear(input);
+    } else if (!operators.includes(input[0]) && operationArgs.length <= 1) {
+      clear(operationArgs);
+      clear(operationRslt);
+    }
+  }
+  
+}
 
-const add = function(a, b) {
+function getResult() {
+  if (operationArgs.length === 2 && operationArgs[1] === "=") {
+    operationRslt[0] = operationArgs[0];
+    operationArgs.pop();
+  }
+  if (operationArgs.length > 3 && operationArgs[1] === "+") {
+    if (operationArgs[3] === "=") {
+      operationRslt[0] = add(operationArgs[0], operationArgs[2]);
+      clear(input); clear(operationArgs); clear(inputClone);
+      operationArgs[0] = operationRslt[0];
+    } else {
+      operationRslt[0] = add(operationArgs[0], operationArgs[2]);
+    clear(input); clear(operationArgs);
+    operationArgs[0] = operationRslt[0];
+    operationArgs[1] = inputClone[inputClone.length - 1];
+    clear(inputClone);
+    }
+  }
+  if (operationArgs.length > 3 && operationArgs[1] === "-") {
+    if (operationArgs[3] === "=") {
+      operationRslt[0] = subtract(operationArgs[0], operationArgs[2]);
+      clear(input); clear(operationArgs); clear(inputClone);
+      operationArgs[0] = operationRslt[0];
+    } else {
+      operationRslt[0] = subtract(operationArgs[0], operationArgs[2]);
+    clear(input); clear(operationArgs);
+    operationArgs[0] = operationRslt[0];
+    operationArgs[1] = inputClone[inputClone.length - 1];
+    clear(inputClone);
+    }
+  }
+}
+
+function add(a, b) {
     let rslt = a + b;
     return rslt;
   };
   
-  const subtract = function(a, b) {
+ function subtract(a, b) {
     let rslt = a - b;
     return rslt;
   };
