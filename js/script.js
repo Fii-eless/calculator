1d06e9f312ee00ade65c2cc48e6bff31eab6cf52
fii-eless <fiifiad@gmail.com>
2023-01-25 01:04:01 +0400

    Format factorial, multiplication and division operations

--- js/script.js
+++ js/script.js
@@ -25,24 +25,25 @@ function returnKey(e) {
     if (!key && !special) return;
     if (e.key === "Backspace") {
       input.splice(input.indexOf("Backspace"), 2);
-      inputClone.splice(inputClone.indexOf("Backspace"), 2)
-    };
+      inputClone.splice(inputClone.indexOf("Backspace"), 2);
+    }
     if (!key) {input.push(e.key); inputClone.push(e.key);}
     if (key) {input.push(e.key); inputClone.push(e.key);}
     if (input.includes("Enter")) {input[input.indexOf("Enter")] = "="; inputClone[inputClone.indexOf("Enter")] = "=";}
-    if (e.key === "Backspace") {backSpace(input); backSpace(inputClone);}
-    
     formatInput(input);
     getResult();
     if (e.key === "Escape") {clear(input); clear(inputClone); clear(operationArgs); clear(operationRslt)}
+    if (e.key === "Backspace") {
+      backSpace(input); backSpace(inputClone);
+      if (!input[0]) operationArgs.pop();
+    }
     console.log(input);
-    // console.log(inputClone);
+    console.log(inputClone);
     console.log(operationArgs);
     console.log(operationRslt);
 }
 buttons.forEach(button => button.addEventListener("click", logXter));
 function logXter(e) {
-  // if (!this.textContent) return;
   input.push(this.textContent);
   if (input.includes("×")) input[input.indexOf("×")] = "*";
   if (input.includes("÷")) input[input.indexOf("÷")] = "/";
@@ -51,7 +52,10 @@ function logXter(e) {
   if (input.includes("X!")) input[input.indexOf("X!")] = "!";
   if (input.includes("+/-")) input[input.indexOf("+/-")] = "-";
   if (this.id === "clearAll") clear(input);
-  if (this.id === "backspace") backSpace2(input);
+  if (this.id === "backspace") {
+    backSpace2(input); backSpace2(inputClone);
+    if (!input[0]) operationArgs.pop();
+  }
   console.log(input)
 }
 function backSpace(arr) {
@@ -65,12 +69,12 @@ function clear(arr) {
 }
 function formatInput(input) {
   for (let i=0; i < input.length; i++) {
-    if (input[0] === "=" && operationArgs.length > 1) {
+    if (input[0] === "=" && operationArgs.length >= 1) {
       operationRslt[0] = operationArgs[0];
       clear(operationArgs); clear(input);
     } else if (input[0] === "=") {
-      operationRslt[0] = 0;
-      clear(input);
+        operationRslt[0] = 0;
+        clear(input);
     }  else if (arithM.includes(input[0]) && !operationArgs[0]) {
         operationArgs[0] = 0; operationArgs.push(input[0]);
         clear(input);
@@ -81,15 +85,15 @@ function formatInput(input) {
         operationArgs[0] = 1; operationArgs.push(input[0]);
         clear(input);
     } else if (geoM.includes(input[0]) && operationArgs[0]) {
-      operationArgs.push(input[0]);
-      clear(input);
+        operationArgs.push(input[0]);
+        clear(input);
     } else if (operators.includes(input[i]) || equator.includes(input[i])) { 
         operationArgs.push(parseFloat(input.splice(0, input.length - 1).join("")));
         operationArgs.push(input[input.length -1]);
         clear(input);
     } else if (!operators.includes(input[0]) && operationArgs.length <= 1) {
-      clear(operationArgs);
-      clear(operationRslt);
+        clear(operationArgs);
+        clear(operationRslt);
     }
   }
   
@@ -100,21 +104,41 @@ function getResult() {
     operationRslt[0] = operationArgs[0];
     operationArgs.pop();
   }
+  if (operationArgs[1] === "+" && operationArgs[2] === "+" || operationArgs[1] === "+" && geoM.includes(operationArgs[2])) {
+    clear(input); clear(inputClone); clear(operationArgs); clear(operationRslt)
+    operationRslt[0] = "SYNTAX ERROR!";
+  }
+  if (operationArgs.length > 3 && operationArgs[2] === "-" && !operators.includes(operationArgs[3]) ) {
+    let negNum = parseFloat(operationArgs[2] + operationArgs[3]);
+    if (typeof negNum === "number") {
+     operationArgs[2] = negNum;
+    } else {
+      clear(input); clear(inputClone); clear(operationArgs); clear(operationRslt)
+      operationRslt[0] = "SYNTAX ERROR!";
+    }
+  }
+  if (operationArgs.length === 2 && operationArgs[1] === "!") {
+      operationRslt[0] = factorial(operationArgs[0]);
+      clear(input); clear(operationArgs);
+      operationArgs[0] = operationRslt[0];
+      clear(inputClone);
+    // }
+  }
   if (operationArgs.length > 3 && operationArgs[1] === "+") {
-    if (operationArgs[3] === "=") {
+    if (operationArgs[3] === "=" || operationArgs[4] === "=") {
       operationRslt[0] = add(operationArgs[0], operationArgs[2]);
       clear(input); clear(operationArgs); clear(inputClone);
       operationArgs[0] = operationRslt[0];
     } else {
       operationRslt[0] = add(operationArgs[0], operationArgs[2]);
-    clear(input); clear(operationArgs);
-    operationArgs[0] = operationRslt[0];
-    operationArgs[1] = inputClone[inputClone.length - 1];
-    clear(inputClone);
+      clear(input); clear(operationArgs);
+      operationArgs[0] = operationRslt[0];
+      operationArgs[1] = inputClone[inputClone.length - 1];
+      clear(inputClone);
     }
   }
   if (operationArgs.length > 3 && operationArgs[1] === "-") {
-    if (operationArgs[3] === "=") {
+    if (operationArgs[3] === "=" || operationArgs[4] === "=") {
       operationRslt[0] = subtract(operationArgs[0], operationArgs[2]);
       clear(input); clear(operationArgs); clear(inputClone);
       operationArgs[0] = operationRslt[0];
@@ -126,6 +150,48 @@ function getResult() {
     clear(inputClone);
     }
   }
+  if (operationArgs.length > 3 && operationArgs[1] === "*") {
+    if (operationArgs[3] === "=" || operationArgs[4] === "=") {
+      operationRslt[0] = multiply(operationArgs[0], operationArgs[2]);
+      clear(input); clear(operationArgs); clear(inputClone);
+      operationArgs[0] = operationRslt[0];
+    } else {
+      operationRslt[0] = multiply(operationArgs[0], operationArgs[2]);
+      clear(input); clear(operationArgs);
+      operationArgs[0] = operationRslt[0];
+      operationArgs[1] = inputClone[inputClone.length - 1];
+      clear(inputClone);
+    }
+  }
+  if (operationArgs.length > 3 && operationArgs[1] === "/") {
+    if (operationArgs[3] === "=" || operationArgs[4] === "=") {
+      operationRslt[0] = divide(operationArgs[0], operationArgs[2]);
+      clear(input); clear(operationArgs); clear(inputClone);
+      operationArgs[0] = operationRslt[0];
+    } else {
+      operationRslt[0] = divide(operationArgs[0], operationArgs[2]);
+      clear(input); clear(operationArgs);
+      operationArgs[0] = operationRslt[0];
+      operationArgs[1] = inputClone[inputClone.length - 1];
+      clear(inputClone);
+    }
+  }
+  if (operationArgs.length > 3 && operationArgs[1] === "^") {
+    if (operationArgs[3] === "=" || operationArgs[4] === "=") {
+      operationRslt[0] = power(operationArgs[0], operationArgs[2]);
+      clear(input); clear(operationArgs); clear(inputClone);
+      operationArgs[0] = operationRslt[0];
+    } else {
+      operationRslt[0] = power(operationArgs[0], operationArgs[2]);
+      clear(input); clear(operationArgs);
+      operationArgs[0] = operationRslt[0];
+      operationArgs[1] = inputClone[inputClone.length - 1];
+      clear(inputClone);
+    }
+  }
+}
+function displayInput() {
+
 }
 
 function add(a, b) {
@@ -146,14 +212,14 @@ function add(a, b) {
     return added;
   }
   
-  const multiply = function([...arr]) {
-    let args = arr;
-    let times = args.reduce((total, arg) => {
-      return total *= arg;
-    }, 1);
+  const multiply = function(a, b) {
+    let times = a * b;
     return times;
   }
-  
+  const divide = function(a, b) {
+    let fraction = a / b;
+    return fraction;
+  }
   const power = function(a, b) {
     let exp = a ** b;
       return exp;
@@ -166,4 +232,3 @@ function add(a, b) {
     }
     return fact;
   };
-  
